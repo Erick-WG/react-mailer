1ddbcf1a42d5537cbd35c2e97c778c1f91551840
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2025-01-04 01:43:54 +0300

    Feat: added the email sending functionality
    I used emailjs services to power the email sending routine

--- client-mailer/src/components/Forms/Form.jsx
+++ client-mailer/src/components/Forms/Form.jsx
@@ -1,24 +1,67 @@
 // eslint-disable-next-line
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './form.css';
+// import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
     // handle state here
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
+    const [contact, setContact] = useState('');
     const [message, setMessage] = useState('');
+    // const [sent, setSent] = useState(false);
     // eslint-disable-next-line
     const [form, setForm] = useState(null);
+
+    // handle side effects here.
+    useEffect(() => {
+        emailjs.init({
+            publicKey: `${import.meta.env.VITE_EMAILJS_PUBLIC_KEY}`,
+            // Do not allow headless browsers
+            blockHeadless: true,
+            limitRate: {
+              // Set the limit rate for the application
+              id: 'app',
+              // Allow 1 request per 10s
+              throttle: 10000,
+            },
+        });
+    }, []);
+
 
     // handle form submission here
     const handleSubmit = (e) => {
         e.preventDefault();
-        const newForm = { name: name, email: email, subject: subject, message: message };
+        // setSent(true);
+
+        const newForm = {
+            from_name: name,
+            sender_email: email,
+            sender_contact: contact,
+            message: message,
+            to_name: `${import.meta.env.VITE_TO_NAME}`,
+            reply_to: `${import.meta.env.VITE_EMAILJS_REPLY_TO}`
+        };
+
         setForm(newForm);
-        // console.log(form);
-        // console.log(newForm);
+
+        emailjs.send(`${import.meta.env.VITE_EMAILJS_SERVICE_ID}`, `${import.meta.env.VITE_EMAILJS_TEMPLATE_ID}`, newForm)
+        .then((result) => {
+                console.log(result.text);
+                alert("Message sent successfully");
+                setName('');
+                setEmail('');
+                setContact('');
+                setMessage('');
+                // setSent(false);
+                // console.log(sent)
+            }, (error) => {
+                console.log(error.text);
+                // setSent(false);
+                alert("Message not sent");
+        });
     }
 
     // handle form input changes here
@@ -31,8 +74,8 @@ const Form = () => {
             case 'email':
                 setEmail(value);
                 break;
-            case 'subject':
-                setSubject(value);
+            case 'contact':
+                setContact(value);
                 break;
             case 'message':
                 setMessage(value);
@@ -44,7 +87,7 @@ const Form = () => {
 
   return (
     <div className="form-container">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         {/* contacts */}
             <input 
                 type="text"
@@ -62,9 +105,9 @@ const Form = () => {
             />
             <input 
                 type="text"
-                name="subject"
-                value={subject}
-                placeholder="Subject"
+                name="contact"
+                value={contact}
+                placeholder="+123 78898798"
                 onChange={handleChange}
             />
         {/* message */}
@@ -74,8 +117,9 @@ const Form = () => {
                 placeholder="Message"
                 onChange={handleChange}
             ></textarea>
-            <button 
+            <button
                 type="submit"
+                // disabled={sent ? false : true}
                 onClick={handleSubmit}
                 onKeyDown={handleSubmit}
             >Send</button>
